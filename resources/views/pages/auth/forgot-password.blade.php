58dba6ba7dff33435afd6194bb52308b7fb0f819
softwarepk <softwarepk@users.noreply.github.com>
2026-09-03 16:59:40 +0500

    Use Laravel 13 namespaced auth layout

--- resources/views/pages/auth/forgot-password.blade.php
+++ resources/views/pages/auth/forgot-password.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.auth :title="__('Forgot password')">
+<x-layouts::auth :title="__('Forgot password')">
     <div class="flex flex-col gap-6">
         <x-auth-header :title="__('Forgot your password?')" :description="__('Enter your email and we will send a reset link')" />
         <x-auth-session-status class="text-center" :status="session('status')" />
@@ -9,4 +9,4 @@
         </form>
         <div class="text-center"><flux:link :href="route('login')" wire:navigate>{{ __('Back to login') }}</flux:link></div>
     </div>
-</x-layouts.auth>
+</x-layouts::auth>
